34387024af08a74437d4977bd0f4e6bbb8958f6e
Amanuel Ewnetu <aewnetu6@gmail.com>
2023-07-22 10:25:42 +0300

    error response function

--- json.go
+++ json.go
@@ -7,6 +7,16 @@ import (
 	"net/http"
 )
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	if code > 499 {
+		log.Printf("Responding with 5XX error: %v", msg)
+	}
+	type ErrorResponse struct {
+		Error string `json:"error"`
+	}
+	respondWithJSON(w, code, ErrorResponse{Error: msg})
+}
+
 func respondWithJSON(w http.ResponseWriter, code int, response interface{}) {
 	data, err := json.Marshal(response)
 	if err != nil {
