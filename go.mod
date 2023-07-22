3a5bddd8bdacafe30c968065fd41cb831f0eff4a
Amanuel Ewnetu <aewnetu6@gmail.com>
2023-07-22 10:15:14 +0300

    setup server

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/manny-e1/rss_aggregator
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load()
+	PORT := os.Getenv("PORT")
+	if PORT == "" {
+		log.Fatal("PORT not found in the enviroment")
+	}
+
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + PORT,
+	}
+	log.Printf("Server started listening on %v", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		fmt.Println(err)
+	}
 }
