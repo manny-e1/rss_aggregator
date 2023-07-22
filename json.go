f18d70870d6b223af49e81a4b39dee9282d92c2a
Amanuel Ewnetu <aewnetu6@gmail.com>
2023-07-22 10:39:05 +0300

    migration and sqlc generate user

--- json.go
+++ json.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -23,7 +22,6 @@ func respondWithJSON(w http.ResponseWriter, code int, response interface{}) {
 		log.Printf("Error occuring trying to marshal respone: %v", response)
 		return
 	}
-	fmt.Println(data)
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
 	w.Write(data)
